f1d344cde565312d41768ade986b119af242e7bf
Administrator <lingboling1991@126.com>
2016-01-15 00:38:27 +0800

    半自主半抄袭，做了一道之前不会做的题

--- src/try_again/maximum_product_of_word_lengths.java
+++ src/try_again/maximum_product_of_word_lengths.java
@@ -2,12 +2,13 @@ package try_again;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 
 /**
  * Created by Administrator on 2015/12/22.
  */
 public class maximum_product_of_word_lengths {
-    public static int maxProduct(String[] words) {
+    public static int maxProduct_1(String[] words) {
 
         Arrays.sort(words, new Comparator<String>() {
             @Override//可以写Comparator来实现自定义规则的比较
@@ -26,21 +27,75 @@ public class maximum_product_of_word_lengths {
         return 1;
     }
 
-    public static void main(String[] args) {
+    public static int maxProduct_2(String[] words) {
+        Arrays.sort(words, new Comparator<String>() {
+            @Override//可以写Comparator来实现自定义规则的比较
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
 
-        String[] words = {"ab", "nn", "ac"};
+        int len = words.length;
+        boolean isSharing = false;
+        int res = 0;
 
-        int[] masks = new int[words.length];
-
-        for (int i = 0; i < words.length; i++) {
-            for (char c : words[i].toCharArray()) {
-                int tmp = c - 'a';
-                int tmp2 = 1 << tmp;
-                masks[i] = masks[i] | tmp2;
-//                masks[i] |= 1 << (c - 'a');
+        for (int i = len - 1; i >= 0; i--) {
+            HashSet<Character> longestHashSet = new HashSet<>();//当前最长的一个字符串，放到HashSet
+            char[] longestChars = words[i].toCharArray();
+            for (int j = 0; j < longestChars.length; j++) {
+                longestHashSet.add(longestChars[j]);
+            }
+            for (int j = i - 1; j >= 0; j--) {//从当前这个往前看
+                char[] curChars = words[j].toCharArray();
+                for (int k = 0; k < curChars.length; k++) {
+                    if (longestHashSet.contains(curChars[k])) {
+                        isSharing = true;
+                        break;
+                    }
+                }
+                if (!isSharing && longestChars.length * curChars.length > res) {
+                    res = longestChars.length * curChars.length;
+                }
+                isSharing = false;
             }
         }
+        return res;
+    }
 
-        System.out.println("aa");
+    public static int maxProduct(String[] words) {
+
+        //这里不需要排序，反正是要都算一遍，因为有可能出现a,ab,cd,bcd这种情况，所以不能从最大的倒着遍历
+        int len = words.length;
+        boolean isSharing = false;
+        int res = 0;
+
+        for (int i = len - 1; i >= 0; i--) {
+            char[] lastChars = words[i].toCharArray();
+            int[] lastCount = new int[26];
+            for (char c : lastChars) lastCount[c - 'a']++;//这里不要用HashSet，因为开销太大，用int[]也能标记
+
+            for (int j = i - 1; j >= 0; j--) {//从当前这个往前看
+                char[] curChars = words[j].toCharArray();
+
+                for (int k = 0; k < curChars.length; k++) {
+                    if (lastCount[curChars[k] - 'a'] != 0) {
+                        //curChars[k]是当前字符串的第k个字符，lastCount[curChars[k] - 'a']是这个字符在longestChars中的个数
+                        isSharing = true;
+                        break;
+                    }
+                }
+                if (!isSharing && lastChars.length * curChars.length > res) {
+                    res = lastChars.length * curChars.length;
+                }
+                isSharing = false;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String[] x = {"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+
+        System.out.println(maxProduct(x));
     }
 }
